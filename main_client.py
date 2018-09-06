9368ba85ebc3fba569f4275603cbd16c67dba6f5
Julien Thieffry <julien.thieffry@eamslab.com>
2018-09-06 17:25:57 +0900

    main_client.py: adding main client script

---
+++ main_client.py
@@ -0,0 +1,72 @@
+"""
+Main handler for the client component.
+
+Usage: python3 main_client.py --address ADDRESS [--frames FRAMES]
+                             [--sleep SLEEP] [--rate RATE]
+"""
+
+
+import argparse
+
+import socks
+from camera import Camera
+
+def main(): #pylint: disable=too-many-locals, too-many-statements
+    """Main function for client loop."""
+
+    # PYTHON PARSER VIA ARGPARSE #
+
+    parser = argparse.ArgumentParser(description="AWS FaceDetect Client Main.")
+    parser.add_argument("-a", "--address", required=True,
+                        help="Address to connect to.", type=str)
+    args = vars(parser.parse_args())
+
+    server_addr = args['address']
+
+    # FACEDETECT CLIENT #
+
+    print(" ------------------")
+    print("| DARKARM - CLIENT |")
+    print(" ------------------")
+
+    print("\n Initializing ENV variables...", end='')
+    capture_loc = socks.init_environ_folder()
+    print("Done!")
+
+    print("\n Initializing server socket...", end='')
+    client_socket = socks.init_client_socket(server_addr)
+    print("Done!")
+
+    print("\n **** CAPTURING FRAME ****")
+    cam = Camera(path=capture_loc)
+    cam.capture()
+    print("Frame captured!")
+
+    print("\n **** SENDING FRAME ****")
+    frame_loc = capture_loc + "frame.jpg"
+    socks.send_frame_size(client_socket, frame_loc)
+    socks.send_frame(client_socket, frame_loc)
+    print("Done.")
+    print("Waiting for frame reception...", end='')
+    socks.waiting_for_ack(client_socket)
+    print("ACK")
+
+    print("\nFrame sent!")
+
+    print("\n **** RECEIVING VECTOR ****")
+
+    vector = socks.receive_bytes_to_string(client_socket)
+
+    print("\nVector received!")
+
+    print("\n **** GENERATING VIDEO ****")
+
+    print("Vector is: " + str(vector))
+
+    print("\n ------------------------")
+    print("| DARKARM CLIENT - GOODBYE |")
+    print(" --------------------------")
+
+
+if __name__ == '__main__':
+    main()
