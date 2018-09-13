7f09867a5114e7a8bbd5971b9cd64b2030e48896
Julien Thieffry <julien.thieffry@eamslab.com>
2018-09-13 11:26:24 +0900

    main.py: setting srv loop and detecting bad detection output

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Usage: python3 main.py [--sleep SLEEP]
 """
 
 import argparse
+import os
 
 import socks
 import dark
@@ -35,41 +36,51 @@ def main(): # pylint: disable=too-many-statements
     server_socket = socks.init_server_socket()
     print("Done!")
 
-    server_socket.listen(5)
-    print("\nWaiting for incoming connection...")
-    client, addr = server_socket.accept()
-    print("Incoming connection from " + str(addr))
+    while True:
+        server_socket.listen(5)
+        print("\nWaiting for incoming connection...")
+        client, addr = server_socket.accept()
+        print("Incoming connection from " + str(addr))
 
-    print("\n **** RECEIVING FRAME ****")
+        print("\n **** RECEIVING FRAME ****")
 
-    frame_size = int(socks.receive_bytes_to_string(client))
-    socks.send_msg(client, 'OK FRAME')
-    socks.receive_frame(client, frame_size, inbox_loc)
-    print("Frame  received.")
+        frame_size = int(socks.receive_bytes_to_string(client))
+        socks.send_msg(client, 'OK FRAME')
+        socks.receive_frame(client, frame_size, inbox_loc)
+        print("Frame  received.")
 
-    print("Label detection...")
-    vect = {}
-    output = dark.get_detection_output(darkarm_loc, inbox_loc, label, 'frame')
-    rect_center = dark.compute_center(dark.parse_detection_output(output))
-    coord = dark.get_translation_vec(inbox_loc + "frame.jpg", rect_center)
-    vect['xval'] = coord[0]
-    vect['yval'] = coord[1]
+        print("Label detection...")
+        vect = {}
+        try:
+            output = dark.get_detection_output(darkarm_loc, inbox_loc, label, 'frame')
+            output = output.decode('utf-8')
+            sep = r'\d'
+            rest = output.split(sep, 1)[0]
+            print(rest)
+            rect_center = dark.compute_center(dark.parse_detection_output(rest))
+            print(rect_center)
+            coord = dark.get_translation_vec(inbox_loc + "frame.jpg", rect_center)
+            vect['xval'] = coord[0]
+            vect['yval'] = coord[1]
+        except KeyError:
+            print("No detected output")
 
-    print("Sending ack...", end='')
-    socks.send_msg(client, 'OK FRAME')
-    print("Sent.")
+        print("Sending ack...", end='')
+        socks.send_msg(client, 'OK FRAME')
+        print("Sent.")
 
-    print("\nFrame processing completed!")
+        print("\nFrame processing completed!")
 
-    print("\n **** SENDING VECTOR ****")
-    socks.send_msg(client, vect)
+        print("\n **** SENDING VECTOR ****")
+        socks.waiting_for_ack(client, "VECT")
+        socks.send_msg(client, vect)
 
-    print("\nVector sent!")
+        print("\nVector sent!")
 
-    print("Closing sockets...", end='')
-    client.close()
-    server_socket.close()
-    print("Done!")
+        print("Closing sockets...", end='')
+        client.close()
+        print("Done!")
+        # os.remove('server/inbox/frame.jpg')
 
     print("\n ------------------------")
     print("| DARKARM SERVER - GOODBYE |")
